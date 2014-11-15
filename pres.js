78ff3b24b67680096023ad0d26e3861fd96e5da8
Kevin Reid <kpreid@switchb.org>
2014-11-15 10:56:35 -0800

    Demonstrate frequency shifting.

--- pres.js
+++ pres.js
@@ -65,6 +65,15 @@ define(['../../client/widget'], function (widget) {
     };
   }
   
+  function Multiply(iqin1, iqin2, iqout) {
+    var limit = Math.min(iqin1.length, iqin2.length, iqout.length);
+    return function rotator() {
+      for (var i = 0; i < limit; i += 2) {
+        iqout[i]   = iqin1[i] * iqin2[i] - iqin1[i+1] * iqin2[i+1];
+        iqout[i+1] = iqin1[i+1] * iqin2[i] + iqin1[i] * iqin2[i+1];
+      }
+    };
+  }
   function Rotator(iqin, iqout, radiansPerSample) {
     var limit = Math.min(iqin.length, iqout.length);
     return function rotator() {
@@ -77,6 +86,19 @@ define(['../../client/widget'], function (widget) {
         phase += radiansPerSample;
       }
       //phase = phase % TWOPI;
+    };
+  }
+  function Siggen(iqout, radiansPerSampleFn) {
+    var limit = iqout.length;
+    return function siggen() {
+      var phase = 0;
+      var radiansPerSample = radiansPerSampleFn();
+      for (var i = 0; i < limit; i += 2) {
+        var c = cos(phase);
+        iqout[i]   = cos(phase);
+        iqout[i+1] = sin(phase);
+        phase += radiansPerSample;
+      }
     };
   }
   function Interpolator(iqin, iqout) {
@@ -108,12 +130,17 @@ define(['../../client/widget'], function (widget) {
   var ambuf = new Float32Array(sampleCount * 2);
   var hfbuf = new Float32Array(interpolation * sampleCount * 2);
   var amout = new Float32Array(interpolation * sampleCount * 2);
+  var demodrot = new Float32Array(interpolation * sampleCount * 2);
+  var product = new Float32Array(interpolation * sampleCount * 2);
   var g = Graph([
     AMModulator(audioarray, ambuf),
     Interpolator(ambuf, hfbuf),
-    Rotator(hfbuf, amout, 0.15)
+    Rotator(hfbuf, amout, 0.15),
+    Siggen(demodrot, function() { return (mbdirector ? Math.min(mbdirector.clock(demodStep) * 0.08, 1) : 0) * -0.15; }),
+    Multiply(amout, demodrot, product),
   ]);
   
+  var mbdirector, demodStep = 4;
   ThreeBox.preload(['../../client/mathbox.glsl.html'], goMathbox);
   function goMathbox() {
     var element = document.getElementById('mb');
@@ -175,7 +202,7 @@ define(['../../client/widget'], function (widget) {
     
     // wave
     function docurve(id, color, array) {
-      mathbox.curve({
+      return {
         id: id,
         color: color,
         n: array.length / 2,
@@ -183,10 +210,11 @@ define(['../../client/widget'], function (widget) {
         domain: [-timeRangeScale, timeRangeScale],
         expression: IQPlotter(array),
         lineWidth: 2,
-      })
+      }
     }
-    docurve('modulatingam', 0x000000, ambuf);
-    docurve('am', 0x0077FF, amout);
+    mathbox.curve(docurve('modulatingam', 0x000000, ambuf));
+    mathbox.curve(docurve('am', 0x0077FF, product));
+    mathbox.curve();
     
     var step0 = [
       'Amplitude modulation (AM)',
@@ -195,11 +223,11 @@ define(['../../client/widget'], function (widget) {
     var script = [
       [
         'Amplitude modulation (AM)',
-        'The problem with this picture, for our purposes, is that the math is messy. For example, if you were trying to demodulate this, every time the signal crosses zero, you have no data because the audio was multiplied by zero. Of course, in reality the carrier frequency is immensely higher than the audio frequency, so it\'s easy to average over that. It\'s not that it\'s infeasible to work this way — you can, in exact analogy to analog RF electronics, but rather that there\'s something else you can do which is much more elegant all around.'
+        'The problem with this picture, for our purposes, is that the math is messy. For example, if you were trying to demodulate this, every time the signal crosses zero, you have no data because the audio was multiplied by zero. Of course, in reality the carrier frequency is immensely higher than the audio frequency, so it\'s easy to average over that. It\'s not that it\'s infeasible to work this way — you can, in exact analogy to analog RF electronics, but rather that there\'s something else you can do which is much more elegant all around. It doesn\'t matter as much for AM, but I\'m using AM in this picture because it makes good pictures, not because it\'s a good example.'
       ],
       [
         'Analytic signals',
-        'Here we have a signal which has values which are complex numbers rather than real numbers. The carrier wave is a complex sinusoid. The modulation works exactly the same way ',
+        'Here we have a signal which has values which are complex numbers rather than real numbers. The carrier wave is a complex sinusoid. The modulation works exactly the same way — multiplying the complex carrier by the real audio scales the magnitude of the carrier. (We will see later how this picture corresponds to physical radio signals.)',
         ['set', '#iaxis', {labels: true}],
         ['set', '#qaxis', {labels: true}],
         ['animate', 'camera', {
@@ -209,18 +237,23 @@ define(['../../client/widget'], function (widget) {
           delay: 0,
           duration: 6000
         }],
-        //['animate', 'camera', {
-        //  phi: Math.PI * 0.6,
-        //  //theta: Math.PI * 0.3
-        //}, {
-        //  delay: 4000,
-        //  duration: 2000
-        //}],
       ],
+      [
+        'Frequency selection and demodulation',
+        'So how do we demodulate this analytic signal? For AM, it turns out we can simply take the magnitude of these complex signals and we\'re done. But in general, the first step is to undo the effect of the carrier.',
+        ['remove', '#modulatingam']
+      ],
+      [
+        'Frequency selection and demodulation',
+        'We do this by multiplying the signal by another complex sinusoid, shown in red, of equal and opposite frequency — the helix is wound the other way.',
+        ['add', 'curve', docurve('demodrot', 0xFF0000, demodrot)],
+        //['animate', '#demodrot', { /* dummy */ }, {
+        //  duration: 1000
+        //}]
+      ]
     ];
     var mbscript = script.map(function(step) { return step.slice(2); });
-    console.log(mbscript);
-    var mbdirector = new MathBox.Director(mathbox, mbscript);
+    mbdirector = new MathBox.Director(mathbox, mbscript);
     
     document.body.addEventListener('keydown', function (event) {
       if (event.keyCode == 39) {
@@ -229,15 +262,15 @@ define(['../../client/widget'], function (widget) {
       if (event.keyCode == 37) {
         mbdirector.back();
       }
-      console.log(event.keyCode, mbdirector.step);
+      console.log('Now at step ', mbdirector.step);
     }, false);
     //setTimeout(function() {
     //  mbdirector.forward();
     //}, 1000);
+    mbdirector.go(3);
     
     setInterval(function() {
       var step = mbdirector.step;
-      //console.log(step);
       document.getElementById('slidetitle').textContent = (step ? script[step - 1] : step0)[0];
       document.getElementById('slidecaption').textContent = (step ? script[step - 1] : step0)[1];
     }, 20);
